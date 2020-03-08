3277690936f7f77bcdf7c7d13aa89bc97c5e6587
Fleming <ADwithDeveloper@gmail.com>
2020-03-08 22:22:39 +0530

    venue provider

--- src/app/views/venue-provider/venue-provider.module.ts
+++ src/app/views/venue-provider/venue-provider.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VenueProviderRoutingModule } from './venue-provider-routing.module';
 import { VenueProviderComponent } from './venue-provider.component';
+import { DataTableModule } from 'angular2-datatable';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { VenueProviderDataFilterPipe } from './datafilterpipe';
 
 @NgModule({
   imports: [
     CommonModule,
-    VenueProviderRoutingModule
+    VenueProviderRoutingModule,
+    DataTableModule,
+    FormsModule,
+    HttpModule
   ],
-  declarations: [VenueProviderComponent]
+  declarations: [VenueProviderComponent,VenueProviderDataFilterPipe]
 })
 export class VenueProviderModule { }

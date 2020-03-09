b1251e9d0044f4b93dc0faadeada23a072a09982
sahilparmar1810 <sahilparmar1810@gmail.com>
2020-03-09 22:45:37 +0530

    update in profile

--- src/app/views/profile/profile.module.ts
+++ src/app/views/profile/profile.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile.component';
 import { ProfileRoutingModule } from './profile-routing.module';
+import { FormsModule } from '@angular/forms';
+import { BsDatepickerModule } from 'ngx-bootstrap';
+import { SelectModule } from 'ng-select';
 
 @NgModule({
   imports: [
     CommonModule,
-    ProfileRoutingModule
+    ProfileRoutingModule,
+    FormsModule,
+    SelectModule,
+
   ],
   declarations: [ProfileComponent]
 })

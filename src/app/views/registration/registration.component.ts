e9e4c2a06c400fe4fd63adeb46e184a60911012a
Fleming <ADwithDeveloper@gmail.com>
2020-03-10 17:08:01 +0530

    add chnages

--- src/app/views/registration/registration.component.ts
+++ src/app/views/registration/registration.component.ts
@@ -6,10 +6,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
-
+  public data: any;
+  public user:any={};
+  public userRoles=[
+    {label:"Admin", value:"admin"},
+    {label:"User", value:"user"},
+    {label:"Venue Provider", value:"venue provider"}
+  ];
+  public genders= [
+    {label:"Male", value:"male"},
+    {label:"Female", value:"female"}
+  ]
   constructor() { }
 
   ngOnInit() {
+    
+    this.data = [
+      {
+        img:"assets/img/xs/avatar1.jpg",
+        name:"jhon smith",
+        phone:"264-625-2583",
+        email:"johnsmith@gmail.com",
+        address:"123 6th St. Melbourne, FL 32904",
+        userType:"Admin"
+      },
+      {
+        img:"assets/img/xs/avatar2.jpg",
+        name:"Hossein Shams	",
+        phone:"264-625-5689",
+        email:"hosseinshams@gmail.com",
+        address:"44 Shirley Ave. West Chicago, IL 60185",
+        userType:"User"
+      },
+      {
+        img:"assets/img/xs/avatar3.jpg",
+        name:"Maryam Amiri",
+        phone:"264-625-9513",
+        email:"maryamamiri@gmail.com",
+        address:"123 6th St. Melbourne, FL 32904",
+        userType:"Venue Provider"
+      },
+      {
+        img:"assets/img/xs/avatar4.jpg",
+        name:"Tim Hank",
+        phone:"264-625-1212",
+        email:"timhank@gmail.com",
+        address:"70 Bowman St. South Windsor, CT 06074",
+        userType:"Venue Provider"
+      },
+
+
+    ]
   }
 
 }
--- src/app/views/registration/registration.module.ts
+++ src/app/views/registration/registration.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegistrationRoutingModule } from './registration-routing.module';
 import { RegistrationComponent } from './registration.component';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
+import { DataTableModule } from 'angular2-datatable';
+import { ModalModule } from 'ngx-bootstrap';
+import { RegistrationDataFilterPipe } from './datafilterpipe';
 
 @NgModule({
   imports: [
     CommonModule,
-    RegistrationRoutingModule
+    RegistrationRoutingModule,
+    ModalModule.forRoot(),
+    DataTableModule,
+    FormsModule,
+    NgSelectModule,
     
   ],
-  declarations: [RegistrationComponent]
+  declarations: [RegistrationComponent,RegistrationDataFilterPipe]
 })
 export class RegistrationModule { }
